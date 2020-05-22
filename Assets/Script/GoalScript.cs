2a562a576957c9d494873e820650ba6363d78111
yanagyi <yanagyi429@gmail.com>
2020-05-22 15:47:54 +0900

    潜水艦に触れるとクリアのロゴ出現する処理（ステージ1のみ実装）

---
+++ Assets/Script/GoalScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour {
+
+    //クラス生成
+    public GameObject ClearText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //通常時は見えていない
+        ClearText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //プレイヤーがゴールに触れると
+        if(other.gameObject.tag == "Player")
+        {
+            //クリアのロゴが出現
+            ClearText.SetActive(true);
+        }
+    }
+}
