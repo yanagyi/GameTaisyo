c91a2317b02228d8ee9392749f0f589a1adfa383
yanagyi <yanagyi429@gmail.com>
2020-05-25 10:33:24 +0900

    ステージ間のステージ移行処理実装

--- Assets/Script/GoalScript.cs
+++ Assets/Script/GoalScript.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class GoalScript : MonoBehaviour {
 
     //クラス生成
+
+    //クリアテキストクラス生成
     public GameObject ClearText;
+    //シーンチェンジクラス生成
+    public StageChange stageChange;
 
     // Start is called before the first frame update
     void Start()
     {
         //通常時は見えていない
         ClearText.SetActive(false);
+        
+        //シーンチェンジのコンポーネント取得
+        stageChange = GameObject.Find("StageChange").GetComponent<StageChange>();
     }
 
     // Update is called once per frame
@@ -27,6 +34,9 @@ public class GoalScript : MonoBehaviour {
         {
             //クリアのロゴが出現
             ClearText.SetActive(true);
+
+            //次のステージに移行
+            stageChange.NextStage();
         }
     }
 }
