a967c4cfe17eb422cce2681f301c2b8d0f530dd9
thepooryi <thepooryi@gmail.com>
2020-06-05 17:00:06 +0900

    上下左右水流と続く時間

--- Assets/Script/MoveDestory.cs
+++ Assets/Script/MoveDestory.cs
@@ -4,20 +4,53 @@ using UnityEngine;
 
 public class MoveDestory : MonoBehaviour
 {
+   
+    [SerializeField] private int direction = 1;
+    [SerializeField] private float seconds = 3.0f;
+
+    
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(transform.gameObject, 4);//三秒後Destory
+
+       
+
+        Destroy(transform.gameObject, seconds);//三秒後Destory
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.down * Time.deltaTime * 2);//落下
+       
+       
+        if (direction == 0)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * 2);//上
+        }
+
+        if (direction == 1)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * 2);//下
+        }
+
+        if (direction == 2)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 2);//左
+        }
+
+        if (direction == 3)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 2);//右
+        }
+
     }
+
+    
 
 
 
